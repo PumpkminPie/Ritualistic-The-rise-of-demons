fba9a4fc542aaeb074d5fc08362fb2a83686893d
João Artur Pereira Silva <sjoaoartursilva@gmail.com>
2026-02-16 02:36:39 -0300

    Basic cam system (n fui mimir ainda ;{ )

--- Assets/Scripts/Player/Player_CamSystem.cs
+++ Assets/Scripts/Player/Player_CamSystem.cs
@@ -1,6 +1,26 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player_CamSystem : MonoBehaviour
 {
-    
+    [SerializeField] Transform playerBody;
+    [SerializeField] float camMoveFactor = 10f;
+
+    [SerializeField] Camera mainCamera;
+    [SerializeField] float mainCamMaxZ;
+
+    private void Start()
+    {
+        if (playerBody == null) 
+            mainCamera = Camera.main;
+
+        mainCamMaxZ = mainCamera.transform.position.z;
+    }
+
+    void Update()
+    {
+        Vector3 goTo = new Vector3(playerBody.position.x, playerBody.position.y, mainCamMaxZ);
+
+        transform.position = Vector3.Lerp(transform.position, goTo, camMoveFactor * Time.deltaTime);
+    }
 }
